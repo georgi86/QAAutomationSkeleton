59490cabcd6564aa0c367e517226881ae2e2d258
Georgi Vatashki <georgi.vatashki@sirma.bg>
2017-05-24 23:26:07 +0300

    Added notes for Resizable Constants

--- Tests/AutomationTests/Pages/ResizablePage/ResizablePageConstants.cs
+++ Tests/AutomationTests/Pages/ResizablePage/ResizablePageConstants.cs
@@ -7,14 +7,14 @@
         internal const int WAIT_SECCONDS = 20;
 
         //// Selectors
-        internal const string DEFAULT_TAB_SELECTOR = "ui-id-1";
-        internal const string CONSTRAINT_RESIZE_TAB_SELECTOR = "ui-id-3";
-        internal const string MIN_MAX_SIZE_TAB_SELECTOR = "ui-id-5";
-        internal const string DEFAULT_RESIZABLE_BOX = "resizable";
-        internal const string DEFAULT_RESIZE_ANGLE = "//*[@id=\"resizable\"]/div[3]";
-        internal const string CONSTRAINT_RESIZABLE_BOX = "resizableconstrain";
-        internal const string CONSTRAINT_RESIZE_ANGLE = "//*[@id=\"resizableconstrain\"]/div[3]";
-        internal const string MIN_MAX_RESIZABLE_BOX = "resizable_max_min";
-        internal const string MIN_MAX_RESIZE_ANGLE = "//*[@id=\"resizable_max_min\"]/div[3]";
+        internal const string DEFAULT_TAB_SELECTOR = "ui-id-1";                                    //This is a main Tab
+        internal const string CONSTRAINT_RESIZE_TAB_SELECTOR = "ui-id-3";                          //This is a main tab
+        internal const string MIN_MAX_SIZE_TAB_SELECTOR = "ui-id-5";                               //This is a main tab
+        internal const string DEFAULT_RESIZABLE_BOX = "resizable";                                 //This will be use only in the assetrt logic. 
+        internal const string DEFAULT_RESIZE_ANGLE = "//*[@id=\"resizable\"]/div[3]";              //This is an lower right angle and will be use for resizing.
+        internal const string CONSTRAINT_RESIZABLE_BOX = "resizableconstrain";                     //This will be use only in the assetrt logic.    
+        internal const string CONSTRAINT_RESIZE_ANGLE = "//*[@id=\"resizableconstrain\"]/div[3]";  //This is an lower right angle and will be use for resizing.
+        internal const string MIN_MAX_RESIZABLE_BOX = "resizable_max_min";                         //This will be use only in the assetrt logic.
+        internal const string MIN_MAX_RESIZE_ANGLE = "//*[@id=\"resizable_max_min\"]/div[3]";      //This is an lower right angle and will be use for resizing.
     }
 }
